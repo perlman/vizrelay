5dfec5e0bb4003a5eabe28e95d53dbad053ef0ca
Eric Perlman <perlman@gmail.com>
2018-04-20 14:05:11 -0700

    Minor cleanup. Add render port and protocol.

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ Settings (default) management for viz redirection service.
 '''
 
 from argschema import ArgSchema, ArgSchemaParser
-from argschema.fields import List, NumpyArray, Bool, Int, Nested, Str
+from argschema.fields import Nested, Str, Int
 from argschema.schemas import DefaultSchema
 from argschema.utils import args_to_dict, smart_merge
 
@@ -11,6 +11,10 @@ DEFAULT_SETTINGS = {
     'neuroglancer' : {
         'base_url' : 'http://ibs-forrestc-ux1.corp.alleninstitute.org:8002',
         'blend' : 'additive'
+    },
+    'render' : {
+        'protocol' : 'http',
+        'port' : 80
     }
 }
 
@@ -18,12 +22,15 @@ class NeuroglancerSchema(DefaultSchema):
     base_url = Str(default="https://neuroglancer-demo.appspot.com/", help="Neuroglancer URL", required=False)
     blend = Str(default="default", help="Default blend mode (default or additive)", required=False)
 
+class RenderSchema(DefaultSchema):
+    protocol = Str(default="http", help="Protocol to connect to render with (http or https)", required=False)
+    port = Int(default=80, required=False)
 
 class VizRelaySchema(ArgSchema):
     neuroglancer = Nested(NeuroglancerSchema, required=True)
+    render = Nested(RenderSchema, required=True)
 
-def get_settings(query_args):  # {'a.b.c.' : 'd'} {'--a.b.c}
-
+def get_settings(query_args):
     d = {}
     smart_merge(d, DEFAULT_SETTINGS)
     smart_merge(d, args_to_dict(query_args, schema=VizRelaySchema()))
